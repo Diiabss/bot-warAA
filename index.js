32edcfcbf01f42876b4922615bd95791ec5ffd7c
Abdillahi Ahamada <a.bd.illahi@hotmail.fr>
2025-08-01 10:22:30 +0200

    Correction pour la connexion à l'api

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const { decideAction } = require('./bot');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
+
+app.options('/action', cors()); 
 
 app.get('/action', (req, res) => {
   const decision = decideAction(); 
